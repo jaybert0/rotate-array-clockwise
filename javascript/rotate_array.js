32b8d8813b33d38fc05b45a2218a781fd5cc05da
J Lee <jay.j.lee.1@gmail.com>
2022-05-03 14:50:10 -0400

    done

--- javascript/rotate_array.js
+++ javascript/rotate_array.js
@@ -1,5 +1,15 @@
 function rotateArray(arr, k) {
   // type your code here
+  if (k > arr.length) {
+    k = k%arr.length
+  }
+  const idx = arr.length - k
+  newArr = arr.splice(idx)
+
+  const superArr = newArr.concat(arr)
+  return superArr
+  // console.log(superArr)
+  // arr = [1,2,3]; k=2; arr.length = 3. idx = (arr.length - k) = 1. newarr = arr.splice(1) = [2,3]. arr.unshift(newarr)
 }
 
 if (require.main === module) {
@@ -21,4 +31,7 @@ if (require.main === module) {
 module.exports = rotateArray;
 
 // Please add your pseudocode to this file
+// splice from (arr.length - k - 1) to form new array and then shift from end of new arr back into previous arr.
+// arr= [1, 2, 3, 4]; k=1 . arr.length = 4. i =(arr.length - k ) = 3. newarr = arr.splice(3) = [4]. arr.unshift(newarr)
+// arr = [1,2,3]; k=2; arr.length = 3. i = (arr.length - k) = 1. newarr = arr.splice(1) = [2,3]. arr.unshift(newarr)
 // And a written explanation of your solution
